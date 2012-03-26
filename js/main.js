985a49011a40a7702f1f43d47e26d9c5a26aa64d
Razvan Secara <razvan.secara@softvision.ro>
2012-03-26 17:42:18 +0300

    stop at 3000. added animation. fixed some issues

--- js/main.js
+++ js/main.js
@@ -1,140 +1,126 @@
 // How many rounds were played
-var round = 0
+var round = 1
+var active_animation = false
+
+// Tresholds
+var final_score_treshold = 3000 // End game score
+var first_roll_treshold  = 300 // First roll score limit
+
+var players        = []
+var current_player = null
+var winner         = null
+var tie_players    = []
 
 var _default_points = {
-	"1": {"triple":1000, "single":100},
-	"2": {"triple":200,  "single":0},
-	"3": {"triple":300,  "single":0},
-	"4": {"triple":400,  "single":0},
-	"5": {"triple":500,  "single":50},
-	"6": {"triple":600,  "single":0},
+  "1": {"triple":1000, "single":100},
+  "2": {"triple":200,  "single":0},
+  "3": {"triple":300,  "single":0},
+  "4": {"triple":400,  "single":0},
+  "5": {"triple":500,  "single":50},
+  "6": {"triple":600,  "single":0},
 }
 
-var default_dice_faces = [1, 2, 3, 4, 5, 6]
+var dice_faces = [1, 2, 3, 4, 5, 6]
 
 function Player(id, name){
   this.id    = id
   this.name  = name
   this.score = 0
-  this.pending_score  = 0
-  this.remaining_dice = default_dice_faces
+  this.pending_score         = 0
+  this.remaining_dice_number = 5 // default
 }
 
 Player.prototype.update_table_with_score = function(){
-  console.log(this.pending_score)
   $("#player_" + this.id + "_score").html(this.score)
 }
 
-// Comment
-generate_dice_pool = function(dice_faces){
-  dice_pool = []
-  initial_total = dice_faces.length
-  final_total   = initial_total > 5 ? 5 : initial_total
-
-  for (var i=0; i<initial_total; i++){
-    face = dice_faces[i]
-    for (var j=0; j<final_total; j++){
-      dice_pool.push(face)
-    }    
-  }
-  return dice_pool
-}
-
-bind_roll_buttons = function(){  
-  $("#roll-button").click(function(){
-    rolled_dice = roll(current_player.remaining_dice)
-    process_rolled_dice(rolled_dice, false)
-  });
-  $("#re-roll-button").click(function(){
-    rolled_dice = roll(current_player.remaining_dice)
-    process_rolled_dice(rolled_dice, true)
-  });
-}
-
-// array_of_faces
-roll = function(array_of_faces){
-  dice_pool = generate_dice_pool(array_of_faces) //don't generate this everytime
-  total     = array_of_faces.length > 5 ? 5 : array_of_faces.length // TODO: Refactor
-  
-  return dice_pool.sample(total)
+// TODO: xyz
+roll = function(number_of_dice){
+  return dice_faces.multiply(number_of_dice).sample(number_of_dice)
 }
 
 process_rolled_dice = function(rolled_dice, re_roll){
-    var _dice_images     = []
-    var _processed_picks = []
-    var _score           = 0
-    var _remaining_dice  = []
-    _dice_frequency = dice_by_frequency(rolled_dice)
+  var _dice_images     = []
+  var _processed_picks = []
+  //var _colored_dice    = {}
+  var _score           = 0
+  var _remaining_dice_number  = 0
+  _dice_frequency = rolled_dice.to_frequency()
+
+  for (var i=0; i < rolled_dice.length; i++){
+    _pick   = rolled_dice[i]
+    _points = _default_points[_pick]
+    _partial_results = partial_score(_dice_frequency[_pick.toString()], _points.triple, _points.single)
+
+    if (_processed_picks.indexOf(_pick) == -1){
+      _score += _partial_results.score
+      _processed_picks.push(_pick)
+    }
     
-    for (var i=0; i < rolled_dice.length; i++){
-      _pick   = rolled_dice[i]
-      _points = _default_points[_pick]
-      _partial_results = partial_score(_dice_frequency[_pick], _points.triple, _points.single)
-      
-      if (_processed_picks.indexOf(_pick) == -1){
-        _score += _partial_results.score
-        _processed_picks.push(_pick)
-      }      
-      
-      _added_in_score = (_partial_results.triple != 0 || _partial_results.rest != 0)
-      if (_added_in_score == false) _remaining_dice.push(_pick)
-      // BUG : 4 two's, 4 three's
-      _dice_images.push(generate_dice_face(_pick, _added_in_score))
+    //if (_partial_results.triple != 0) abc = 3 
+    _should_color = (_partial_results.triple != 0 || _partial_results.rest != 0)
+    
+    // if (_should_color){
+      // if (_colored_dice[_pick.toString()] == undefined){
+        // //console.log(_pick + " does not exist")
+        // _colored_dice[_pick.toString()] = 1
+      // }else{
+        // //console.log("~" + _colored_dice[_pick.toString()])
+        // //console.log("~" + _dice_frequency[_pick.toString()])
+        // if (_colored_dice[_pick.toString()] == _dice_frequency[_pick.toString()]){
+          // //console.log(_pick + " EQL")
+          // _should_color = false
+        // }else{
+          // ///console.log(_pick + " Increment")
+          // _colored_dice[_pick.toString()]++
+        // }
+      // }
+    // } else {
+      // console.log("WTF")
+      // _remaining_dice_number++
+    // }
+    // console.log("-------------")
+    // BUG : 4 two's, 4 three's
+    _dice_images.push(generate_dice_face(_pick, _should_color))
+  }
+
+  if (_score > 0) manage_special_buttons("show")
+  
+  current_player.pending_score += _score
+  current_player.remaining_dice_number = _remaining_dice_number
+  
+  $("#results").html(_dice_images.join(" "))
+  $("#rolled-score").html(_score)
+  
+  if (re_roll) {
+    $("#pending-score").html(current_player.pending_score)
+    if (_score == 0) {
+      $(".zero-score-message").show()
+      current_player.pending_score = 0
+      setTimeout(change_player, 3000);
     }
-
-    if (_score > 0) manage_special_buttons("show")
-
-    current_player.pending_score += _score
-    current_player.remaining_dice = _remaining_dice
-
-    $("#results").html(_dice_images.join(" "))
-    $("#rolled-score").html(_score)
-    if (re_roll) {
-      $("#pending-score").html(current_player.pending_score)
-      if (_score == 0) {
-        current_player.pending_score = 0
-        change_player()
-      }
-    }
+  }
 }
 
-generate_dice_face = function(number, scoring_face){
-  if (scoring_face == true){
-    result = '<div class="dice scoring">' +
-                '<p class="single_dice face_' + number + '"/>' +
-              '</div>'
-  }else{
-    result = '<p class="single_dice face_' + number + '"/>'
+generate_dice_face = function(face_number, scoring_face){
+  result = '<p class="single_dice face_' + face_number + '"/>'
+  if (scoring_face){
+    result = '<div class="dice scoring">' + result + '</div>'
   }
 
   return result
 }
 
-// Returns a hash
-dice_by_frequency = function(picks){
-  hashed_picks = {}
-  for (var i = 0; i < picks.length; i++){
-    pick = picks[i].toString()
-    if (hashed_picks[pick] == undefined){
-      hashed_picks[pick] = 1
-    }else{
-      hashed_picks[pick]++
-    }
-  }
-  
-  return hashed_picks
-}
-
 partial_score = function(number_frequency, triple_score, single_score){
-	result_for_triple = Math.floor(number_frequency/3) * triple_score
+  result_for_triple = Math.floor(number_frequency/3) * triple_score
   is_triple = result_for_triple != 0 ? 3 : 0
-	
+
   mod = number_frequency % 3
   result_for_single = mod * single_score
   is_single = result_for_single != 0 ? mod : 0
-  
+
   return {
-    "score": result_for_triple + result_for_single,    
+    "score": result_for_triple + result_for_single,
     "triple": is_triple,
     "rest":   is_single
   }
@@ -161,7 +147,7 @@ collect_players = function(){
     }
     $("#start-game").hide()
     manage_roll_button("show")
-    $(".content").show()
+    $(".playground").show()
     $("#players").html(players_to_html.join(""))
     // set current player
     set_current_player(players["player_1"])
@@ -186,53 +172,124 @@ manage_special_buttons = function(action){
   }
 }
 
-bind_next_player_button = function(){
-  $("#next-player-button").click(function(){
-    change_player()
-  })
-}
-
-change_player = function(){  
-  // Detect the next player
-  next_player = players["player_" + (current_player.id + 1)]
-  if (next_player == undefined) next_player = players["player_1"]
+change_player = function(){
+  // Don't count the score if it's less than 300. This applies to the players with their score = 0
+  // Before starting to sum the scores a score of 300 in a single turn is required
+  if (current_player.score == 0 && current_player.pending_score < first_roll_treshold){
+    current_player.pending_score = 0
+  }
+  current_player.score += current_player.pending_score // add the pending score to the current score
+  current_player.pending_score         = 0   // reset the pending score
+  current_player.remaining_dice_number = 5  // default the number of dice
+  current_player.update_table_with_score() // Update the table containing the scores
   
-  current_player.score += current_player.pending_score
-  current_player.pending_score = 0
-  current_player.remaining_dice = default_dice_faces
-  // Update the table containing the scores
-  current_player.update_table_with_score()
-  // Store the info of the current player into the window.players
+  // Store the info of the current player into the window.players hash.
   window.players["player_" + current_player.id] = current_player
   
-  // Change the current player
-  set_current_player(players["player_" + next_player.id])
-  // Redraw the divs
-  re_draw()
+  // Detect the next player
+  next_player = players["player_" + (current_player.id + 1)]
+  if (next_player == undefined){
+    change_round()
+  }else{
+    set_current_player(players["player_" + next_player.id])
+    re_draw() // Redraw the playing board
+  }
 }
 
-// 
+change_round = function(){
+  game_status = check_if_game_ended()
+  switch(game_status){
+    case false:
+      // Change the current player
+      set_current_player(players["player_1"])
+      round++
+      if (active_animation) { setTimeout(play_round_fight_animation, 500) }
+      re_draw()
+    break;
+    case true:
+      announce_winner()
+    break;
+    case "tie":
+      console.log("TIE")
+    break;
+  }
+}
+
+// TODO: Comment
 set_current_player = function(player){
   window.current_player = player
 }
 
+announce_winner = function(){
+  $(".playing-board").hide()
+  $(".winner").html("<h3>Congratulations " + winner.name + "! You won with " + winner.score + " points.</h3>")
+  $(".winner").show()
+}
+
 //=======================================================
 
+// Redraw the playing board
 re_draw = function(){
-  $(".rolling-player").html(current_player.name)
-  manage_special_buttons("hide")
-  $("#rolled-score").html(0)
-  $("#pending-score").html(0)
-  $("#results").html("")
+  manage_special_buttons("hide") // hide the re-roll & next_player buttons and show the roll one.
+  $(".round").html(round) // update the round
+  $(".rolling-player").html(current_player.name) // update the current rolling player
+  $("#rolled-score").html(0)   // reset the score
+  $("#pending-score").html(0) // reset the pending score
+  $("#results").html("")     // clean the previous dice results
+  $(".zero-score-message").hide()
+}
+
+play_round_fight_animation = function(){
+  $(".round").html(round)
+  $(".overlay").show();
+  $("#round-number").fadeIn(3000, function(){
+    $("#round-number").hide()
+    $("#fight-animation").fadeIn(3000, function(){
+      $("#fight-animation").hide();
+      $(".overlay").fadeOut(1000);
+    });
+  })
+}
+
+/*
+ * Return options: true, false, "tie"
+ */
+check_if_game_ended = function(){
+  tie_players = []
+  for (var player in players){
+    if (players[player].score > final_score_treshold) { tie_players.push(players[player]) }
+  }
+
+  if (tie_players.length > 1){
+    return "tie"
+  }else{
+    if (tie_players.length == 1){
+      winner = tie_players[0]
+      return true
+    }else{
+      return false
+    }
+  }
+}
+
+bind_roll_buttons = function(){
+  $("#roll-button, #re-roll-button").click(function(){
+    rolled_dice = roll(current_player.remaining_dice_number)
+    process_rolled_dice(rolled_dice, this.id == "re-roll-button")
+  });
+  
+  $("#next-player-button").click(function(){
+    change_player()
+  });
 }
 
 bind_start_game = function(){
   $("#start-game").click(function(){
     collect_players()
+    if (active_animation) { setTimeout(play_round_fight_animation, 500) }
     re_draw()
     bind_roll_buttons()
-    bind_next_player_button()
-  })  
+  })
 }
 
 $(document).ready(function(){
--- js/misc.js
+++ js/misc.js
@@ -2,16 +2,44 @@ Array.prototype.sample = function(n){
   if (typeof n == "undefined") n = 1
   
   results = []
-  for (var i = 0; i < n; i++){  
+  for (var i = 0; i < n; i++){
     results.push(this[Math.floor(Math.random()*this.length)])
   }
 
   return results
 }
-Storage.prototype.setObject = function(key, value) {
-    this.setItem(key, JSON.stringify(value));
+
+// Returns a hash (number:frequency) having the numbers as keys and their frequency as values
+Array.prototype.to_frequency = function(){
+  hashed_picks = {}
+  for (var i = 0; i < this.length; i++){
+    pick = this[i].toString()
+    if (hashed_picks[pick] == undefined){
+      hashed_picks[pick] = 1
+    }else{
+      hashed_picks[pick]++
+    }
+  }
+  
+  return hashed_picks
 }
 
-Storage.prototype.getObject = function(key) {
-    return JSON.parse(this.getItem(key));
+// Multiply the array by a number of times dictated by the 'n' parameter
+Array.prototype.multiply = function(n){
+  results = []
+  for (var i = 0; i < this.length; i++){
+    for (var j = 0; j < n; j++){
+      results.push(this[i])
+    }
+  }
+
+  return results
 }
+
+// Storage.prototype.setObject = function(key, value) {
+  // this.setItem(key, JSON.stringify(value));
+// }
+// 
+// Storage.prototype.getObject = function(key) {
+  // return JSON.parse(this.getItem(key));
+//}
